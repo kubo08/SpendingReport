7836fdaf926fcff20998f40e7c16b9f6c65539d9
kubo08 <susedik.jakub@gmail.com>
2015-12-16 17:24:40 +0100

    new jquery ui

---
+++ spending report/Helpers/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PagedList;
+using SpendingReport.Models;
+using SpendingReport.Helpers;
+
+namespace SpendingReport
+{
+    public static class Extensions
+    {
+        public static IPagedList<TSource> ToPagedList<TSource>(this IEnumerable<TSource> source, int pageNumber, int pageSize)
+        {
+            if (source == null) throw new Exception("source is empty");
+            return new PagerList<TSource>(source, pageNumber, pageSize, source.Count());
+
+        }
+
+        public static IPagedList<Transaction> ToPagedList(this TransactionsModel source, int pageNumber, int pageSize)
+        {
+            if (source == null) throw new Exception("source is empty");
+            return new PagerList<Transaction>(source.Transactions, pageNumber, pageSize, source.TotalItems);
+
+        }
+    }
+}
---
+++ spending report/ViewModels/TransactionsModel.cs
@@ -0,0 +1,13 @@
+﻿using PagedList;
+using SpendingReport.Models;
+using SpendingReport.Helpers;
+
+namespace SpendingReport.ViewModels
+{
+    public class TransactionsListModel
+    {
+        public Pager Pager { get; set; }
+
+        public IPagedList<Transaction> TransactionsList { get; set; }
+    }
+}
