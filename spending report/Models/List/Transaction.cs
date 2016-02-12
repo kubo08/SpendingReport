de85a90fb01e867fd921593d77f774d3cc383d7b
kubo08 <susedik.jakub@gmail.com>
2016-02-12 14:46:28 +0100

    pridanie suborov ktore neboli v source control

---
+++ spending report/Models/List/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpendingReport.Models
+{
+    public class Transaction
+    {
+        public BankAccount BankAccount { get; set; }
+
+        public TransactionAmount TransacionAmount { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? DatePosted { get; set; }
+
+        public DateTime? DateAvailable { get; set; }
+
+        public string SpecificSymbol { get; set; }
+
+        public string VariableSymbol { get; set; }
+
+        public string ConstantSymbol { get; set; }
+    }
+}
---
+++ spending report/Models/List/TransactionAmount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpendingReport.Models
+{
+    public class TransactionAmount
+    {
+        public decimal Amount { get; set; }
+
+        public string Currency { get; set; }
+
+        public string TransactionType { get; set; }
+
+        public string PaymentType { get; set; }
+    }
+}
