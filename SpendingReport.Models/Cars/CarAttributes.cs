bd5682d37016d5eb99bbc81cf0e403bab717055a
kubo08 <susedik.jakub@gmail.com>
2016-02-12 14:05:14 +0100

    pridanie vypadnuteho suboru

---
+++ SpendingReport.Models/Cars/CarAttributes.cs
@@ -0,0 +1,15 @@
+﻿namespace SpendingReport.Service.Models.Cars
+{
+    public class CarAttributes
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int? TankSize { get; set; }
+
+        public string Description { get; set; }
+
+        public string Color { get; set; }
+    }
+}
